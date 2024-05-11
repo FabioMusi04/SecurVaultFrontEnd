b68ea6dbadc353b5701901ffc2114fc21b2f81c8
FabioMusi04 <76118020+FabioMusi04@users.noreply.github.com>
2024-05-11 11:00:52 +0200

    feat: Finished page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,42 @@
 import { Image, Container, Text, Button, Grid, Title, Card } from '@mantine/core';
-
+import { useNavigate } from 'react-router-dom';
 export function HomePage() {
+    const navigate = useNavigate();
     document.title = "Home | Startup Landing";
-    document.body.style.background = "linear-gradient(to right bottom, #262b51 75%, #403077 25%) no-repeat center center fixed";
+    document.body.style.background = "linear-gradient(to right bottom, #7078f4, #db6b80) no-repeat center center fixed";
 
     return (
         <div style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             <Container size="lg" style={{ textAlign: 'center'}} fluid>
-                <Card style={{ marginBottom: '20px', boxShadow: '0 0 300px black' }} bg="transparent">
+                <Card style={{ marginBottom: '20px', boxShadow: '0 0 300px black' }} bg="linear-gradient(to right bottom, #3f2f76, #252b50 50%, #3f2f76) no-repeat center center fixed">
                     <Grid>
                         <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
-                            <Title size="h1" fw={700}>
-                                Startup
+                            <Title order={1} size="h1" fz={75} fw={700} style={{
+                                textShadow:
+                                    "-1px -1px 0 black, 0   -1px 0 black,1px -1px 0 black,1px  0   0 black,1px  1px 0 black, 0    1px 0 black,-1px  1px 0 black,-1px  0   0 black"
+                            }}>
+                                Secur
                             </Title>
-                            <Title size="h1" fw={700} c="purple">
-                                Landing
+                            <Title order={1} fw={700} fz={75} c="purple" style={{
+                                textShadow:
+                                    "-1px -1px 0 white, 0   -1px 0 white,1px -1px 0 white,1px  0   0 white,1px  1px 0 white, 0    1px 0 white,-1px  1px 0 white,-1px  0   0 white"
+                            }}>
+                                Vault
                             </Title>
                             <Text size="lg" mt={"5%"}>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure doloremque quas dolorum. Quo amet
-                                earum alias consequuntur quam accusamus a quae beatae, odio, quod provident consectetur non
-                                repudiandae enim adipisci?
+                                <Text fw={700}>Secure your passwords with SecurVault. </Text> 
+                                SecurVault is a password manager that helps you to store your passwords securely.
+                                It uses the latest encryption technology to protect your data.
+                                <br/><Text fw={700}>The best password manager for your business.</Text>
                             </Text>
                             <Grid mt={"5%"}>
                                 <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
-                                    <Button size="lg" fullWidth>
+                                    <Button size="lg" fullWidth onClick={() =>  navigate("/login")}>
                                         Get Started
                                     </Button>
                                 </Grid.Col>
                                 <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
-                                    <Button size="lg" fullWidth variant="light">
+                                    <Button size="lg" fullWidth variant="light" onClick={() =>  navigate("/learnmore")}>
                                         Learn More
                                     </Button>
                                 </Grid.Col>
@@ -39,7 +47,7 @@ export function HomePage() {
                                 radius="md"
                                 src="https://placehold.co/600x400"
                                 alt="Startup Landing"
-                                style={{ boxShadow: '0 0 15px black' }}
+                                style={{ boxShadow: '0 0 10px gray' }}
                             />
                         </Grid.Col>
                     </Grid>
